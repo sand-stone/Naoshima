d15eb8288b82bbdfa4f092426b3002ecb55d246a
Sand Stone <sand.m.stone@gmail.com>
2018-01-20 21:17:51 -0800

    integrate photos browser

--- N/Naoshima/AppDelegate.swift
+++ N/Naoshima/AppDelegate.swift
@@ -1,38 +1,45 @@
 import UIKit
+import Photos
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]?) -> Bool {
         // Override point for customization after application launch.
+        let splitViewController = self.window!.rootViewController as! UISplitViewController
+        #if os(iOS)
+            let navigationController = splitViewController.viewControllers.last! as! UINavigationController
+            navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        #endif
+        splitViewController.delegate = self
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    // MARK: Split view
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? AssetGridViewController else { return false }
+        if topAsDetailController.fetchResult == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func splitViewController(_ splitViewController: UISplitViewController, showDetail vc: UIViewController, sender: Any?) -> Bool {
+        // Let the storyboard handle the segue for every case except going from detail:assetgrid to detail:asset.
+        guard !splitViewController.isCollapsed else { return false }
+        guard !(vc is UINavigationController) else { return false }
+        guard let detailNavController =
+            splitViewController.viewControllers.last! as? UINavigationController,
+            detailNavController.viewControllers.count == 1
+            else { return false }
+        
+        detailNavController.pushViewController(vc, animated: true)
+        return true
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
--- N/README.md
+++ N/README.md
@@ -3,7 +3,11 @@
     sudo gem install cocoapods
     pod setup --verbose
 
+    brew install git-lfs
+    
 ## Build instruction
+    git pull
     pod install
+    git lfs pull
     open Naoshima.xcworkspace
 
